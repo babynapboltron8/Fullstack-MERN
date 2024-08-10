f4b520b34c4f8659e5f95058daeec849f197e7a3
Baby Nap Boltron <147931096+babynapboltron8@users.noreply.github.com>
2024-08-10 16:18:11 +0800

    .

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
-        <div className="header-contents">
+        <div className='header-contents'>
           <h2>Order your favourite food here</h2>
           <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisy your cravings and elevate your dining experience, one delicious meal of a time.</p>
           <button>View Menu</button>
